c73fe90a159fee7c73c77a5fc31199eceb17c72d
Bernd Vogt <bernd.vogt@sourcepit.org>
2020-02-02 17:43:24 +0100

    feat: add cli arguments

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,10 @@ use thread_priority::RealtimeThreadSchedulePolicy;
 use thread_priority::ThreadPriority;
 use thread_priority::ThreadSchedulePolicy;
 
+const ARG_REDIS_HOST: &str = "redis-host";
+const ARG_REDIS_PORT: &str = "redis-port";
+const ARG_NAME: &str = "name";
+const ARG_GPIO_PIN: &str = "gpio-pin";
 const ARG_VERBOSITY: &str = "verbosity";
 const ARG_QUIET: &str = "quiet";
 
@@ -74,6 +78,38 @@ fn run() -> Result<()> {
                 .takes_value(false)
                 .required(false),
         )
+        .arg(
+            Arg::with_name(ARG_REDIS_HOST)
+                .long(ARG_REDIS_HOST)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("localhost"),
+        )
+        .arg(
+            Arg::with_name(ARG_REDIS_PORT)
+                .long(ARG_REDIS_PORT)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("6379"),
+        )
+        .arg(
+            Arg::with_name(ARG_NAME)
+                .long(ARG_NAME)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("dht22"),
+        )
+        .arg(
+            Arg::with_name(ARG_GPIO_PIN)
+                .long(ARG_GPIO_PIN)
+                .multiple(false)
+                .takes_value(true)
+                .required(false)
+                .default_value("4"),
+        )
         .get_matches();
 
     let verbosity = args.occurrences_of(ARG_VERBOSITY) as usize + 1;
@@ -86,15 +122,19 @@ fn run() -> Result<()> {
         .quiet(quiet)
         .init()?;
 
+    let redis_host = value_t!(args, ARG_REDIS_HOST, String)?;
+    let redis_port = value_t!(args, ARG_REDIS_PORT, usize)?;
+    let name = value_t!(args, ARG_NAME, String)?;
+    let gpio_pin = value_t!(args, ARG_GPIO_PIN, usize)?;
+
     // requires:
     // sudo setcap cap_sys_nice=ep <file>
     try_upgrade_thread_priority()?;
 
-    let mut redis = redis::Client::open("redis://127.0.0.1/")?.get_connection()?;
+    let mut redis =
+        redis::Client::open(format!("redis://{}:{}", redis_host, redis_port))?.get_connection()?;
 
-    let pin = 4;
-
-    let mut dht22 = Dht22::open(pin)?;
+    let mut dht22 = Dht22::open(gpio_pin)?;
     loop {
         match dht22.read_data() {
             DhtResult::Data(data) => {
@@ -102,8 +142,8 @@ fn run() -> Result<()> {
                     "Temperature {}°C, Humidity {}%",
                     data.temperature, data.humidity
                 );
-                redis.publish("dht22/temperature", data.temperature)?;
-                redis.publish("dht22/humidity", data.humidity)?;
+                redis.publish(format!("{}/temperature", name), data.temperature)?;
+                redis.publish(format!("{}/humidity", name), data.humidity)?;
             }
             DhtResult::Timeout => debug!("Timeout"),
             DhtResult::ChecksumError => debug!("ChecksumError"),
